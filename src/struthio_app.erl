a9ca5ae4e2ec03a075b6894f61afd4b9867a4142
haruki7049 <tontonkirikiri@gmail.com>
2026-03-19 11:16:50 +0900

    Fix

--- src/struthio_app.erl
+++ src/struthio_app.erl
@@ -6,7 +6,7 @@
 start(_StartType, _StartArgs) ->
     %% Initialize database before starting supervisor
     nostr_db:init(),
-    nostr_relay_sup:start_link().
+    struthio_sup:start_link().
 
 
 stop(_State) ->
